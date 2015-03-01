a1ac93e0049e99d53129def48af11e68e7f9d51f
Arvand Alviri <aalviri@gmail.com>
2015-02-28 17:23:37 -0800

    events are listed in ascending order

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
   def home
   	@event = Event.new
 
-  	@eventsinfuture = Event.future
+  	@eventsinfuture = Event.future.sort_by {|event| event.start_date } #this makes it all in ascending order by start_date
   end
 
   def contact
